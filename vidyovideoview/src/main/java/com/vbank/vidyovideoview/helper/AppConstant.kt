de8215329d4b238c9a9e7214198af43e1c6f99e4
Jayachandran V <jayachandran.venugopalan@bpmlinks.com>
2021-05-28 14:50:33 +0530

    response date and time from api and displayed it in XMl 1

--- vidyovideoview/src/main/java/com/vbank/vidyovideoview/helper/AppConstant.kt
+++ vidyovideoview/src/main/java/com/vbank/vidyovideoview/helper/AppConstant.kt
@@ -1,7 +1,7 @@
 package com.vbank.vidyovideoview.helper
 
 object AppConstant {
-    const val BASE_URL ="http://88e87c881950.ngrok.io/vbank/api/"
+    const val BASE_URL ="https://8vhcp36gvc.execute-api.us-east-1.amazonaws.com/dev/vbank/api/ "
     const val NOTIFICATION_ID ="NOTIFICATION_ID"
     const val CallBackURL = "https://your_document_is_uploaded.imfast.io/?event=signing_complete"
     const val MOVE_TO_USER_INPUT_SCREEN = "MOVE_TO_USER_INPUT_SCREEN"
